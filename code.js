aced05f8354dc5761dd31a3242f697973d104117
YounisLOL07 <Khelyounis123@gmail.com>
2025-03-24 09:30:24 +0100

    small changes

--- code.js
+++ code.js
@@ -1,16 +1,18 @@
-function changeBackground() {
-    let color = document.getElementById("bgColor").value;
+document.getElementById("bgColorSubmit").addEventListener("click", function() {
+    let color = document.getElementById("bgColor").value
     document.body.style.backgroundColor = color;
-}
+});
+
 function addElement() {
     let elementType = document.getElementById("elementType").value;
     let content = document.getElementById("elementContent").value;
     let color = document.getElementById("elementColor").value;
     let parent = document.getElementById(document.getElementById("parentElement").value);
+
     let newElement = document.createElement(elementType);
     newElement.textContent = content;
     newElement.style.color = color;
-    newElement.classList.add("generated");
+
 
     parent.appendChild(newElement);
 }
