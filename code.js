77dd7944959787308f3e7be57212c26e6809fd65
YounisLOL07 <Khelyounis123@gmail.com>
2025-03-25 11:27:28 +0100

    Testing

--- code.js
+++ code.js
@@ -16,3 +16,22 @@ function addElement() {
 
     parent.appendChild(newElement);
 }
+
+let felt = document.getElementById("knappe-felt")
+let erik_knapp = document.createElement("button");
+erik_knapp.innerText = "Erik Knapp";
+erik_knapp.style.color = "red";
+felt.appendChild(erik_knapp);  
+
+erik_knapp.addEventListener("click", function() {
+    var flytt = document.getElementById("erik")
+    var box = document.getElementById("customContainer")
+    var box2 = document.getElementById("container")
+
+    if (box.contains(flytt)) {
+        box2.appendChild(flytt);
+    } else {
+        box.appendChild(flytt);
+    }
+});
+
